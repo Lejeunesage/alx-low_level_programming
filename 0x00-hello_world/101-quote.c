0639d1073d069e5b2c72d89d2171f6a8339fbdf0
Lejeunesage <geoffroyotegbeye@gmail.com>
2023-06-15 15:43:52 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,10 @@
 #include <string.h>
 #include <sys/syscall.h>
 
-int main(void) {
-    const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int main(void)
+{
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     syscall(SYS_write, STDERR_FILENO, message, strlen(message));
     return 1;
 }
+
